8746af390a5d6b1d87a7a5cc2c0f3a6b9b9f1035
KingNier <kingnieracosta@gmail.com>
2023-12-07 15:33:59 -0500

    Add prefix, postfix and exclude options

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -6,12 +6,14 @@ describe("Test all global functions", () => {
     const fileNames = await loader.getAllFiles(path.resolve("./sample"));
     expect(fileNames).toHaveLength(5);
   });
+
   test("Should load all file names inside a folder recursive", async () => {
     const fileNames = await loader.getAllFilesRecursive(
       path.resolve("./sample")
     );
-    expect(fileNames).toHaveLength(7);
+    expect(fileNames).toHaveLength(8);
   });
+
   test("Should load all modules inside a folder", async () => {
     const fileNames = await loader.loadModules("./sample");
     expect(fileNames).toHaveLength(3);
@@ -19,18 +21,66 @@ describe("Test all global functions", () => {
       expect(item).toHaveProperty("data");
     });
   });
-  test("Should load all modules inside a folder recursive", async () => {
+
+  test("Should load all modules inside a folder c", async () => {
     const fileNames = await loader.loadModules("./sample", {
       recursive: true,
     });
-    expect(fileNames).toHaveLength(5);
+    expect(fileNames).toHaveLength(6);
     fileNames.forEach((item) => {
       expect(item).toHaveProperty("data");
     });
   });
+
   test("Should throw an error if an invalid directory is given", async () => {
     await expect(loader.loadModules("invalid dir")).rejects.toThrow(
       "INVALID PATH"
     );
+  });
+
+  test("Should load all modules inside a folder with prefix", async () => {
+    const fileNames = await loader.loadModules("./sample", {
+      recursive: false,
+      prefix: ["c-", "i"],
+    });
+    expect(fileNames).toHaveLength(2);
+    fileNames.forEach((item) => {
+      expect(item).toHaveProperty("data");
+    });
+  });
+
+  test("Should load all modules inside a folder recursive with postfix", async () => {
+    const fileNames = await loader.loadModules("./sample", {
+      recursive: true,
+      postfix: ["service.js", "ex.js"],
+    });
+    expect(fileNames).toHaveLength(4);
+    fileNames.forEach((item) => {
+      expect(item).toHaveProperty("data");
+    });
+  });
+
+  test("Should load all modules inside a folder recursive with exclude", async () => {
+    const fileNames = await loader.loadModules("./sample", {
+      recursive: true,
+      exclude: ["index.js"],
+    });
+    expect(fileNames).toHaveLength(4);
+    fileNames.forEach((item) => {
+      expect(item).toHaveProperty("data");
+    });
+  });
+
+  test("Should load all modules inside a folder recursive with prefix, postfix, exclude", async () => {
+    const fileNames = await loader.loadModules("./sample", {
+      recursive: true,
+      postfix: ["service.js"],
+      prefix: ["c"],
+      exclude: ["child-2.service.js"],
+    });
+    expect(fileNames).toHaveLength(1);
+    fileNames.forEach((item) => {
+      expect(item).toHaveProperty("data");
+    });
   });
 });
--- src/index.js
+++ src/index.js
@@ -10,19 +10,22 @@ const ERROR_MESSAGE = "INVALID PATH";
  * @param {string} dir - The directory path to load the modules from.
  * @param {Object} opts - Optional options object.
  * @param {boolean} opts.recursive - If true, loads modules recursively from the directory. Default is false.
+ * @param {Array<string>} opts.prefix - An array of file name prefixes to match.
+ * @param {Array<string>} opts.postfix - An array of file name postfixes to match.
+ * @param {Array<string>} opts.exclude - An array of file names to exclude from the search.
  * @return {Promise<Array>} - A promise that resolves to an array of loaded modules.
  * @throws {Error} - Throws an error if the directory cannot be accessed or if loading modules fails.
  */
-async function loadModules(dir, opts) {
+async function loadModules(dir, opts = {}) {
   const recursive = opts?.recursive;
   const fullPath = path.resolve(dir);
 
   try {
     await access(fullPath, constants.F_OK && constants.R_OK);
 
-    const fileNames = !!recursive
-      ? await getAllFilesRecursive(fullPath)
-      : await getAllFiles(fullPath);
+    let fileNames = !!recursive
+      ? await getAllFilesRecursive(fullPath, opts)
+      : await getAllFiles(fullPath, opts);
 
     return fileNames
       .map((item) => tryRequire(item))
@@ -48,34 +51,71 @@ function tryRequire(path) {
 }
 
 /**
- * Recursively retrieves all files in a directory and its subdirectories.
+ * Recursively retrieves all files in a directory and its subdirectories
+ * that match specified criteria.
  *
  * @param {string} dir - The directory path to start the search from.
- * @return {Array<string>} An array of file paths.
+ * @param {Object} opts - An optional object containing search criteria.
+ * @param {Array<string>} opts.prefix - An array of file name prefixes to match.
+ * @param {Array<string>} opts.postfix - An array of file name postfixes to match.
+ * @param {Array<string>} opts.exclude - An array of file names to exclude from the search.
+ * @return {Promise<Array<string>>} - A promise that resolves to an array of file paths.
  */
-async function getAllFilesRecursive(dir) {
-  const children = await readdir(dir, { withFileTypes: true });
+async function getAllFilesRecursive(dir, opts = {}) {
+  const prefix = (opts?.prefix || []).map((item) => item.toLowerCase());
+  const postfix = (opts?.postfix || []).map((item) => item.toLowerCase());
+  const exclude = (opts?.exclude || []).map((item) => item.toLowerCase());
+
   const result = [];
-  for (const child of children) {
-    if (child.isDirectory()) {
-      result.push(...(await getAllFiles(path.join(dir, child.name))));
-    } else {
-      result.push(path.join(dir, child.name));
+
+  async function proccessDirectory(directory) {
+    const children = await readdir(directory, { withFileTypes: true });
+
+    for (const child of children) {
+      if (child.isDirectory()) {
+        await proccessDirectory(path.join(directory, child.name));
+      } else {
+        const fileName = child.name.toLowerCase();
+        if (
+          (!prefix.length || prefix.some((val) => fileName.startsWith(val))) &&
+          (!postfix.length || postfix.some((val) => fileName.endsWith(val))) &&
+          !exclude.includes(fileName)
+        ) {
+          result.push(path.join(directory, child.name));
+        }
+      }
     }
   }
 
+  await proccessDirectory(dir);
   return result;
 }
 
 /**
  * Loads all file of a directory.
  *
- * @param {string} dir - The directory path.
- * @return {Array} - An array containing the paths of all the files in the directory.
+ * @param {string} dir - The directory path to search in.
+ * @param {object} opts - Optional options object.
+ * @param {string[]} opts.prefix - An array of prefixes that file names should start with.
+ * @param {string[]} opts.postfix - An array of postfixes that file names should end with.
+ * @param {string[]} opts.exclude - An array of file names to exclude from the search.
+ * @return {Promise<string[]>} A promise that resolves to an array of file paths.
  */
-async function getAllFiles(dir) {
+async function getAllFiles(dir, opts = {}) {
+  const prefix = (opts?.prefix || []).map((item) => item.toLowerCase());
+  const postfix = (opts?.postfix || []).map((item) => item.toLowerCase());
+  const exclude = (opts?.exclude || []).map((item) => item.toLowerCase());
+
   return (await readdir(dir, { withFileTypes: true }))
-    .filter((item) => !item.isDirectory())
+    .filter((item) => {
+      const fileName = item.name.toLowerCase();
+      return (
+        !item.isDirectory() &&
+        (!prefix.length || prefix.some((val) => fileName.startsWith(val))) &&
+        (!postfix.length || postfix.some((val) => fileName.endsWith(val))) &&
+        !exclude.includes(fileName)
+      );
+    })
     .map((item) => path.join(dir, item.name));
 }
 
